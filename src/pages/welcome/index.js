c7fa7e5bbacaa228400a48aa981e532be5ecd051
TulioChiodi <tuliochiodi@gmail.com>
2018-09-14 20:40:17 -0300

    commit diario

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,34 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
 
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Bem-vindo</Text>
-    <Text style={styles.text}>
-      Para continuar, precisamos que você informe seu usuário do Github
-    </Text>
+export default class Welcome extends Component {
+  static navigationOptions = {
+    header: null,
+  };
 
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuário"
-        underLineColorAndroid="rgba(0, 0, 0, 0)"
-      />
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
 
-      <TouchableOpacity style={styles.button} onPress={() =>{}}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  signIn = () => {
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
 
-Welcome.navigationOptions = {
-  header: null,
-};
+    this.props.navigation.dispatch(resetAction);
+  };
 
-export default Welcome;
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem-vindo</Text>
+        <Text style={styles.text}>
+          Para continuar, precisamos que você informe seu usuário do Github
+        </Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underLineColorAndroid="rgba(0, 0, 0, 0)"
+          />
+
+          <TouchableOpacity style={styles.button} onPress={this.signIn}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
