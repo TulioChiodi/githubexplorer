b32e60a5661a6327630382411243c58b7c16f762
TulioChiodi <tuliochiodi@gmail.com>
2018-09-13 21:29:44 -0300

    commit diario 13/09/2018 Estilizando welcome

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'config/ReactotronConfig';
+import 'config/DevToolsConfig';
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Routes from 'routes';
+
+const App = () => (
+  <Routes />
+);
+
+export default App;
---
+++ src/pages/welcome/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continuar, precisamos que você informe seu usuário do Github
+    </Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underLineColorAndroid="rgba(0, 0, 0, 0)"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() =>{}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+Welcome.navigationOptions = {
+  header: null,
+};
+
+export default Welcome;
