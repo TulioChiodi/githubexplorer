31a45556a8beedd52c0a2ccbbecff79a8408a97a
TulioChiodi <tuliochiodi@gmail.com>
2018-10-09 20:32:20 -0300

    commit diário 09/10/2018 fim da unidade 1

--- src/pages/organizations/index.js
+++ src/pages/organizations/index.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
+import api from 'services/api';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
+import OrganizationItem from './components/OrganizationItem';
+
+import styles from './styles';
 
 export default class Organizations extends Component {
   static navigationOptions = {
     title: 'Organizações',
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
   };
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  }
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({
+      data: response.data,
+      loading: false,
+      refreshing: false,
+    });
+  }
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+      numColumns={2}
+      columnWrapperStyle={styles.columnContainer}
+      onRefresh={this.loadOrganizations}
+      refreshing={this.state.refreshing}
+    />
+  );
   render() {
-    return <View/>;
+    return (
+      <View style={styles.container}>
+        {this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
+      </View>
+    );
   }
 }
--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -11,6 +11,8 @@ import {
   FlatList,
 } from 'react-native';
 
+import RepositoryItem from './components/RepositoryItem';
+
 import styles from './styles';
 
 export default class Repositories extends Component {
@@ -22,6 +24,7 @@ export default class Repositories extends Component {
   state = {
     data: [],
     loading: true,
+    refreshing: false,
   }
 
   componentDidMount() {
@@ -29,23 +32,30 @@ export default class Repositories extends Component {
   }
 
   loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
     const username = await AsyncStorage.getItem('@Githuber:username');
     const response = await api.get(`/users/${username}/repos`);
 
-    this.setState({ data: response.data, loading: false });
+    this.setState({
+      data: response.data,
+      loading: false,
+      refreshing: false,
+    });
   }
 
-  renderListItem = ({ item }) => (
-    <Text>{item.full_name}</Text>
-  )
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />
 
   renderList = () => (
     <FlatList
       data={this.state.data}
       keyExtractor={item => String(item.id)}
       renderItem={this.renderListItem}
+      onRefresh={this.loadRepositories}
+      refreshing={this.state.refreshing}
     />
   );
+
   render() {
     return (
       <View style={styles.container}>
